67301c5274b361b3eb5b1fd6d0638f7bb0e9ad9e
idream68 <hzj199603@163.com>
2021-05-31 09:57:43 +0800

    添加mybatis-plus简单使用方法的依赖文件

---
+++ shiro_redis/src/test/java/com/springboot/demo/shiro_redis/ShiroRedisApplicationTests.java
@@ -0,0 +1,12 @@
+package com.springboot.demo.shiro_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
